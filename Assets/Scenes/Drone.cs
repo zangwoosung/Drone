ea696be669ff990c7782d849238f827f78549777
zangwoosung <54176171+zangwoosung@users.noreply.github.com>
2026-04-25 08:13:13 +0900

    [feat] up

--- Assets/Scenes/Drone.cs
+++ Assets/Scenes/Drone.cs
@@ -5,7 +5,8 @@ public class Drone : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Drone is ready to fly!");
+
     }
 
     // Update is called once per frame
